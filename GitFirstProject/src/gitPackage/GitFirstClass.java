7c66973b883d4642415202558623fd281a3a053a
Guru <gurug.qa@gmail.com>
2017-07-24 11:27:49 +1000

    adding the project for the first time

---
+++ GitFirstProject/src/gitPackage/GitFirstClass.java
@@ -0,0 +1,10 @@
+package gitPackage;
+
+public class GitFirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("My First Git Project");
+	}
+
+}
